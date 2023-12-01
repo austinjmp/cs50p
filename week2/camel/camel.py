edf5d4fb33b5ebc0a34e097681428ac32578bf02
austinjmp <90218555+austinjmp@users.noreply.github.com>
2023-12-01 00:50:04 -0700

    update styling for readability

--- week2/camel/camel.py
+++ week2/camel/camel.py
@@ -1,19 +1,18 @@
-#CS50p camelCase to snake_case converter
+# CS50p camelCase to snake_case converter
 
 
 def main():
-    camel =  input("camelCase: ")
+    camel = input("camelCase: ")
     snake = camel_to_snake(camel)
     print("snake_case: ", snake)
-    
+
 
 def camel_to_snake(n):
-
     snake = ""
 
     # iterate until uppercase then and an underscore
     for i in n:
-        if(i.isupper()):
+        if i.isupper():
             snake += "_" + i
         else:
             snake += i
