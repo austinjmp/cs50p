52da8abccb315716aaf5778548c7eee317e23149
austinjmp <90218555+austinjmp@users.noreply.github.com>
2023-12-08 15:17:21 -0700

    Added nummber checking for leading 0 or trailing letters

--- week2/plates/plates.py
+++ week2/plates/plates.py
@@ -6,19 +6,21 @@ def main():
         print("Invalid")
 
 
+# check if plate is valid, returns invalid if a test is failed
 def is_valid(s):
-    s = s.upper()
-    print(s)
     if checklength(s) is False:
         return False
     elif first_two_chars(s[0:2]) is False:
         return False
     elif punctuation(s) is False:
         return False
+    elif num_check(s) is False:
+        return False
     else:
         return True
 
 
+# check the length of the plate (2-6) characters
 def checklength(plate_length):
     if 2 <= len(plate_length) <= 6:
         return True
@@ -26,10 +28,12 @@ def checklength(plate_length):
         return False
 
 
+# check if first two characters are letters
 def first_two_chars(n):
     return not n.isnumeric()
 
 
+# check if there is any punctuation
 def punctuation(plate_punc):
     punc = ".,!?'  "
     for character in plate_punc:
@@ -37,6 +41,16 @@ def punctuation(plate_punc):
             return False
 
     return True
+
+
+# check that the first number is 0 and there are no trailing letters
+def num_check(plate_nums):
+    plate_nums = plate_nums.lower()
+    remove_letters = plate_nums.lstrip("abcdefghijklmnopqrstuvwxyz")
+    if remove_letters[0] == "0":  # check for leading 0
+        return False
+    else:
+        return remove_letters.isnumeric()  # check for trailing letters
 
 
 main()
