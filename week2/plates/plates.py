1e4b7b79d6b27014871fc7d5b1a7d17a6473ed75
austinjmp <90218555+austinjmp@users.noreply.github.com>
2023-12-08 12:04:32 -0700

    add function to check for punctuation

--- week2/plates/plates.py
+++ week2/plates/plates.py
@@ -7,25 +7,36 @@ def main():
 
 
 def is_valid(s):
-    s = s.upper().strip()
+    s = s.upper()
+    print(s)
     if checklength(s) is False:
         return False
     elif first_two_chars(s[0:2]) is False:
+        return False
+    elif punctuation(s) is False:
         return False
     else:
         return True
 
 
 def checklength(plate_length):
-    if 2 >= len(plate_length) <= 6:
+    if 2 <= len(plate_length) <= 6:
         return True
     else:
         return False
 
 
 def first_two_chars(n):
-    print(n)
     return not n.isnumeric()
+
+
+def punctuation(plate_punc):
+    punc = ".,!?'  "
+    for character in plate_punc:
+        if character in punc:
+            return False
+
+    return True
 
 
 main()
