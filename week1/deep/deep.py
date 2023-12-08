f32dcf5bb01e04712757a6a3714708fa127dd711
austinjmp <90218555+austinjmp@users.noreply.github.com>
2023-12-08 00:35:58 -0700

    formatting update

--- week1/deep/deep.py
+++ week1/deep/deep.py
@@ -3,11 +3,11 @@ def main():
         input(
             "What is the Answer to the Greatest Question of Life, the Universe and Everyhting? "
         )
-        .lower() # set to lowercase
-        .strip() # remove spaces
+        .lower()  # set to lowercase
+        .strip()  # remove spaces
     )
     match answer:
-        case 42 | "42" | "forty-two" | "forty two": 
+        case 42 | "42" | "forty-two" | "forty two":
             print("Yes")
         case _:
             print("No")
